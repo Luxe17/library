e410bfd674c38ee8d9dc7ffb1ec3eb641f33bad2
Luxe17 <hanson1717@live.com>
2021-02-23 21:26:42 -0600

    Add index.html

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const books = document.querySelector("#books");
+
+//Book Constructor
 function Book(title, author, pages, status){
     this.title = title;
     this.author = author;
@@ -6,5 +9,15 @@ function Book(title, author, pages, status){
     this.info = function(){
         let Info = [title, author, pages, status];
         let bookInfo = join(', ');
+    }
+}
+
+//list to store books
+let myLibrary = [];
+
+//Display books from myLibrary
+function addToMyLibrary(){
+    for(x = 0; x < myLibrary.length; x++){
+
     }
 }
